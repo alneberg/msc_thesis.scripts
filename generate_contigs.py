5137273ce6a07c1f91e2a2c340bc6684206d932d
Johannes Alneberg <alneberg@kth.se>
2013-11-21 20:54:56 +0100

    Fraction argument to the sample contigs script

--- generate_contigs.py
+++ generate_contigs.py
@@ -13,9 +13,10 @@ from bu.seq_sampling import sample_all
 
 def main(args):
     """For each dir in sequence_dirs, open each file and sample 
-    80 % from those sequences"""
+    from those sequences"""
     seq_dirs = args.sequence_dirs
     seqs = []
+    fraction = args.fraction
     for seq_dir in os.listdir(seq_dirs):
         seq_dir = os.path.join(seq_dirs,seq_dir)
         for f in os.listdir(seq_dir):
@@ -23,12 +24,14 @@ def main(args):
             if f.endswith(".fna"):
                 with open(os.path.join(seq_dir,f), 'r') as seq_file:
                     original_seqs = SeqIO.parse(seq_file, "fasta")
-                    seqs += sample_all(original_seqs)
+                    seqs += sample_all(original_seqs, fraction)
     SeqIO.write(seqs, args.output_file, "fasta")
 
 if __name__=="__main__":
     parser = ArgumentParser(description=DESC)
     parser.add_argument('sequence_dirs', help="Root path where all sequence files are located")
     parser.add_argument('output_file', help="File where all contigs end up.")
+    parser.add_argument('--fraction', default='0.8',
+                        help="fraction to sample from each sequence")
     args = parser.parse_args()
     main(args)
